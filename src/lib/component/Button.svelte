a4dd0cb10ca3e2000ce5c7c26ff37c76207feb2d
Mulyawan <fr1301@protonmail.com>
2026-06-24 13:33:39 +0700

    initial commit

---
+++ src/lib/component/Button.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  let {label} = $props();
+</script>
+
+<button class="btn">{label}</button>
+
+<style lang="css"></style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import Button from "$lib/component/Button.svelte";
+</script>
+<h1> hai ini halaman about</h1>
+<Button label="berhasil"/>

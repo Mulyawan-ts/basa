99f3f9c4d9b1b7e674e7b59ce7fef685d356266f
Mulyawan <fr1301@protonmail.com>
2026-06-26 10:41:16 +0700

    feat: add navbar component for dashboard page

---
+++ src/lib/component/dashboard/DashboardNavbar.svelte
@@ -0,0 +1,6 @@
+<!-- navbar component for dashboard -->
+<div class="navbar bg-base-100 shadow-sm">
+  <div class="navbar-start"></div>
+  <div class="navbar-center"></div>
+  <div class="navbar-end"></div>
+</div>

d536414772bf611e2405551fbf2906fd6249c50c
Mulyawan <fr1301@protonmail.com>
2026-06-26 11:04:53 +0700

    chore: use prettier for nesting code

--- src/routes/(dashboard)/dashboard/+layout.svelte
+++ src/routes/(dashboard)/dashboard/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import { Toaster } from 'svelte-sonner';
-	let { children } = $props;
+	import DashboardNavbar from '$lib/component/dashboard/DashboardNavbar.svelte';
+	import Sidebar from '$lib/component/dashboard/Sidebar.svelte';
+	let { children } = $props();
 </script>
 
 <Toaster position="top-center" richColors></Toaster>
+<DashboardNavbar></DashboardNavbar>
+<Sidebar></Sidebar>
 
 <main class="dashboard-layout">
 	{@render children()}
--- src/routes/(dashboard)/dashboard/+page.svelte
+++ src/routes/(dashboard)/dashboard/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import Sidebar from "$lib/component/dashboard/Sidebar.svelte";
+
 </script>
 
-<Sidebar></Sidebar>
+
 

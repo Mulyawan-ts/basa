c23bdaa7128601e09439692562f00502a044e5d1
Mulyawan <fr1301@protonmail.com>
2026-06-27 14:08:59 +0700

    feat: add sidebar menu

--- src/routes/(dashboard)/DashboardSidebar.svelte
+++ src/routes/(dashboard)/DashboardSidebar.svelte
@@ -9,8 +9,9 @@
 		<label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
 		<ul class="menu bg-base-200 min-h-full w-80 p-4">
 			<!-- Sidebar content here -->
-			<li><a>Sidebar Item 1</a></li>
-			<li><a>Sidebar Item 2</a></li>
+			<li><a href="/dashboard">Summary</a></li>
+			<li><a href="/dashboard/receipt">Receipt</a></li>
+			<li><a>Payment</a></li>
 		</ul>
 	</div>
 </div>

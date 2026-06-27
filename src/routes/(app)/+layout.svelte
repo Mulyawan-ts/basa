ea3b5a3e58153f8942422c74c08b194c25d17046
Mulyawan <fr1301@protonmail.com>
2026-06-27 07:34:43 +0700

    feat: add comment

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
+	// import css
 	import '../layout.css';
 
+	// import component
 	import Navbar from '$lib/component/Navbar.svelte';
 	import Footer from '$lib/component/Footer.svelte';
 	import { page } from '$app/state';
 
 	let { children } = $props();
+
+	// configuration for login page
 	let isLogin = $derived(page.url.pathname === '/login');
 </script>
 
+<!-- describe navbar component -->
 <Navbar></Navbar>
+
+<!-- descrive main page -->
 <main>
 	{@render children()}
 </main>
 
+<!-- footer disable for login -->
 {#if !isLogin}
 	<Footer></Footer>
 {/if}
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	// import hero component
 	import Hero from '$lib/component/Hero.svelte';
 </script>
 
 <!-- This is home page -->
+
+<!-- describe Hero component in homepage -->
 <Hero></Hero>

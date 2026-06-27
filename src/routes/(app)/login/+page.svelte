43ec1379462d242d5e207581fd0379b5e9f50be9
Mulyawan <fr1301@protonmail.com>
2026-06-27 09:05:04 +0700

    feat: styling login

--- src/routes/(app)/login/+page.svelte
+++ src/routes/(app)/login/+page.svelte
@@ -1,11 +1,30 @@
-<fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+<!-- define login page -->
+<div class=login-wrapper>
+<fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-full max-w-sm border p-4">
 	<legend class="fieldset-legend">Login</legend>
 
-	<label class="label">Email</label>
+	<label class="label mt-2">Email</label>
 	<input type="email" class="input" placeholder="Email" />
 
-	<label class="label">Password</label>
+	<label class="label mt-3">Password</label>
 	<input type="password" class="input" placeholder="Password" />
 
-	<button class="btn btn-neutral mt-4">Login</button>
+	<button class="btn btn-neutral w-full mt-4">Login</button>
 </fieldset>
+</div>
+
+
+<style>
+.login-wrapper{
+  display:flex;
+  align-items: top;
+  justify-content: center;
+  min-height: 100vh;
+  width: 100vw;
+  padding: 1rem;
+}
+
+.label{
+  font-weight: bolder;
+}
+</style>
